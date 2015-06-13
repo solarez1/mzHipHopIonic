3f9a4c56fa76c2dc02be8653eb9ef45fc548ac45
solarez1 <abs2k@msn.com>
2015-06-13 10:06:10 +0100

    RSS Feed Displaying Downloadable Music

--- mzHipHopIonic/mzHipHopIonic/www/js/controllers.js
+++ mzHipHopIonic/mzHipHopIonic/www/js/controllers.js
@@ -1,11 +1,27 @@
 var app = angular.module('starter.controllers', [])
 
-app.controller('DashCtrl', function ($scope) {
+app.controller('DashCtrl', function ($http, $scope) {
 
-    $scope.articles = [ ];
-    $scope.rssFeed = 'http://www.feedyes.com/feed.php?f=rnVsf2z9TuLgLI8m';
+    $scope.init = function () {
+        $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://www.feedyes.com/feed.php?f=rnVsf2z9TuLgLI8m", "num": 50} })
+            .success(function (data) {
+                $scope.rssTitle = data.responseData.feed.title;
+                $scope.rssUrl = data.responseData.feed.feedUrl;
+                $scope.rssSiteUrl = data.responseData.feed.link;
+                $scope.entries = data.responseData.feed.entries;
+                $scope.browse = function (v) {
+                    window.open(v, "_system", "location=yes");
+                }
 
-})
+            })
+            .error(function (data) {
+                console.log("ERROR: " + data);
+            });
+    }
+
+});
+
+
 
 app.controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
