8c389cbb3d5bdaa0a9444340c46728a4e12c3bdd
rezwan <abs2k@msn.com>
2015-06-14 21:35:30 +0100

    Added Music News Feed

--- mzHipHopIonic/mzHipHopIonic/www/js/controllers.js
+++ mzHipHopIonic/mzHipHopIonic/www/js/controllers.js
@@ -3,7 +3,7 @@ var app = angular.module('starter.controllers', [])
 app.controller('DashCtrl', function ($http, $scope) {
 
     $scope.init = function () {
-        $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://www.feedyes.com/feed.php?f=rnVsf2z9TuLgLI8m", "num": 50} })
+        $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://www.feedyes.com/feed.php?f=rnVsf2z9TuLgLI8m", "num": 1000} })
             .success(function (data) {
                 $scope.rssTitle = data.responseData.feed.title;
                 $scope.rssUrl = data.responseData.feed.feedUrl;
@@ -21,15 +21,25 @@ app.controller('DashCtrl', function ($http, $scope) {
 
 });
 
+app.controller('ChatsCtrl', function($http, $scope) {
+    $scope.init = function () {
+        $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://www.music-news.com/rss/news.asp", "num": 1000 } })
+            .success(function (data) {
+                $scope.rssTitle = data.responseData.feed.title;
+                $scope.rssUrl = data.responseData.feed.feedUrl;
+                $scope.rssSiteUrl = data.responseData.feed.link;
+                $scope.entries = data.responseData.feed.entries;
+                $scope.browse = function (v) {
+                    window.open(v, "_system", "location=yes");
+                }
 
+            })
+            .error(function (data) {
+                console.log("ERROR: " + data);
+            });
+    }
 
-app.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
+});
 app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
