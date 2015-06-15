c0ed748d7780035983bb84e2d4231f382ad993c7
rezwan <abs2k@msn.com>
2015-06-15 20:20:13 +0100

    inappbrowser working. I had to add in the inappbrowser plugin via the config.xml

---
+++ mzHipHopIonic/mzHipHopIonic/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,57 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.inappbrowser": "0.6.0",
+    "org.apache.cordova.vibration": "0.3.13",
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.splashscreen": "1.0.0",
+    "org.apache.cordova.statusbar": "0.1.10"
+}
+// BOTTOM OF METADATA
+});
---
+++ mzHipHopIonic/mzHipHopIonic/platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,55 @@
+var app = angular.module('starter.controllers', [])
+
+app.controller('DashCtrl', function ($http, $scope) {
+
+    $scope.init = function () {
+        $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://www.feedyes.com/feed.php?f=rnVsf2z9TuLgLI8m", "num": 1000} })
+            .success(function (data) {
+                $scope.rssTitle = data.responseData.feed.title;
+                $scope.rssUrl = data.responseData.feed.feedUrl;
+                $scope.rssSiteUrl = data.responseData.feed.link;
+                $scope.entries = data.responseData.feed.entries;
+                $scope.browse = function (v) {
+                    window.open(v, "_system", "location=yes");
+                   
+             
+                }
+
+            })
+            .error(function (data) {
+                console.log("ERROR: " + data);
+            });
+    }
+
+});
+
+app.controller('ChatsCtrl', function($http, $scope) {
+    $scope.init = function () {
+        $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://www.music-news.com/rss/news.asp", "num": 1000 } })
+            .success(function (data) {
+                $scope.rssTitle = data.responseData.feed.title;
+                $scope.rssUrl = data.responseData.feed.feedUrl;
+                $scope.rssSiteUrl = data.responseData.feed.link;
+                $scope.entries = data.responseData.feed.entries;
+                $scope.browse = function (v) {
+                    window.open(v, "_system", "location=yes");
+               
+                }
+
+            })
+            .error(function (data) {
+                console.log("ERROR: " + data);
+            });
+    }
+
+});
+app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+app.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
+
