b6c49ac8097cb3ef9bd44232779dbda7289b6b95
rezwan <abs2k@msn.com>
2015-06-15 18:41:35 +0100

    controller changed

--- mzHipHopIonic/mzHipHopIonic/www/js/controllers.js
+++ mzHipHopIonic/mzHipHopIonic/www/js/controllers.js
@@ -10,7 +10,7 @@ app.controller('DashCtrl', function ($http, $scope) {
                 $scope.rssSiteUrl = data.responseData.feed.link;
                 $scope.entries = data.responseData.feed.entries;
                 $scope.browse = function (v) {
-                    window.open(v, "_self", "location=yes", "toolbar=yes");
+                    window.open(v, "_self", "location=yes");
              
                 }
 
@@ -31,7 +31,7 @@ app.controller('ChatsCtrl', function($http, $scope) {
                 $scope.rssSiteUrl = data.responseData.feed.link;
                 $scope.entries = data.responseData.feed.entries;
                 $scope.browse = function (v) {
-                    window.open(v, "_self", "location=yes", "toolbar=yes");
+                    window.open(v, "_self", "location=yes");
                   
                 }
 
