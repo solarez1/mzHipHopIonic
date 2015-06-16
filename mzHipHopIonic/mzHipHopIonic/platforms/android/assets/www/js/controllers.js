c94bfe7d3e8a974ac4036cbdcdd7263f15e9ccaa
rezwan <abs2k@msn.com>
2015-06-16 12:03:38 +0100

    location=no

--- mzHipHopIonic/mzHipHopIonic/platforms/android/assets/www/js/controllers.js
+++ mzHipHopIonic/mzHipHopIonic/platforms/android/assets/www/js/controllers.js
@@ -10,7 +10,7 @@ app.controller('DashCtrl', function ($http, $scope) {
                 $scope.rssSiteUrl = data.responseData.feed.link;
                 $scope.entries = data.responseData.feed.entries;
                 $scope.browse = function (v) {
-                    window.open(v, "_system", "location=yes");
+                    window.open(v, "_system", "location=no");
                    
              
                 }
@@ -32,7 +32,7 @@ app.controller('ChatsCtrl', function($http, $scope) {
                 $scope.rssSiteUrl = data.responseData.feed.link;
                 $scope.entries = data.responseData.feed.entries;
                 $scope.browse = function (v) {
-                    window.open(v, "_system", "location=yes");
+                    window.open(v, "_system", "location=no");
                
                 }
 
