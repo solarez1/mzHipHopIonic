ec21156b91a1341a9fbe3b484c3002b05ba31a3b
rezwan <abs2k@msn.com>
2015-06-30 12:24:49 +0100

    test

---
+++ RecipeBox/RecipeBox/platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,86 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-admob/www/AdMob.js",
+        "id": "cordova-plugin-admob.AdMob",
+        "clobbers": [
+            "window.plugins.AdMob"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/src/windows/NotificationProxy.js",
+        "id": "org.apache.cordova.dialogs.NotificationProxy",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/src/windows/InAppBrowserProxy.js",
+        "id": "org.apache.cordova.inappbrowser.InAppBrowserProxy",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/windows8/SplashScreenProxy.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreenProxy",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/src/windows/StatusBarProxy.js",
+        "id": "org.apache.cordova.statusbar.StatusBarProxy",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.google.admobsdk": "6.12.2",
+    "com.google.playservices": "19.0.0",
+    "cordova-plugin-admob": "2.2.0",
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.inappbrowser": "0.6.0",
+    "org.apache.cordova.splashscreen": "1.0.0",
+    "org.apache.cordova.statusbar": "0.1.10",
+    "org.apache.cordova.vibration": "0.3.13"
+}
+// BOTTOM OF METADATA
+});
---
+++ mzHipHopIonic/mzHipHopIonic/platforms/Windows Phone (Universal)/assets/www/js/controllers.js
@@ -0,0 +1,53 @@
+var app = angular.module('starter.controllers', [])
+
+app.controller('DashCtrl', function ($http, $scope) {
+
+    $scope.init = function () {
+        $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://www.feedyes.com/feed.php?f=rnVsf2z9TuLgLI8m", "num": 1000} })
+            .success(function (data) {
+                $scope.rssTitle = data.responseData.feed.title;
+                $scope.rssUrl = data.responseData.feed.feedUrl;
+                $scope.rssSiteUrl = data.responseData.feed.link;
+                $scope.entries = data.responseData.feed.entries;
+                $scope.browse = function (v) {
+                    window.open(v, "browserframe");
+                   
+             
+                }
+
+            })
+            .error(function (data) {
+                console.log("ERROR: " + data);
+            });
+    }
+
+});
+
+app.controller('ChatsCtrl', function($http, $scope) {
+    $scope.init = function () {
+        $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://www.music-news.com/rss/news.asp", "num": 1000 } })
+            .success(function (data) {
+                $scope.rssTitle = data.responseData.feed.title;
+                $scope.rssUrl = data.responseData.feed.feedUrl;
+                $scope.rssSiteUrl = data.responseData.feed.link;
+                $scope.entries = data.responseData.feed.entries;
+                $scope.browse = function (v) {
+                    window.open(v, "_system", "location=no");
+               
+                }
+
+            })
+            .error(function (data) {
+                console.log("ERROR: " + data);
+            });
+    }
+
+});
+app.controller('ChatDetailCtrl', function($scope) {
+  
+})
+
+app.controller('AccountCtrl', function($scope) {
+
+});
+
